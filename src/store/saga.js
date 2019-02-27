6c0b79661e9fc5152a0ae50ac426e786e1cdba3a
zhaiming <zhaiming@aecg.com.cn>
2019-02-27 14:48:19 +0800

    update

--- src/store/saga.js
+++ src/store/saga.js
@@ -1,14 +1,5 @@
 import { call, put, takeEvery, takeLatest } from 'redux-saga/effects'
 import Case from 'case';
-// worker Saga: will be fired on USER_FETCH_REQUESTED actions
-// export function* fetchUser(action) {
-//   try {
-//     const user = yield call(Base.getUsers, action.payload.userId);
-//     yield put({type: "USER_FETCH_SUCCEEDED", user: user});
-//   } catch (e) {
-//     yield put({type: "USER_FETCH_FAILED", message: e.message});
-//   }
-// }
 
 const transEvent = (listener, stateKey) => {
   const eventType = Case.constant(listener.name)
